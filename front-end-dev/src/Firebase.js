b8461efd8a4ef65bf77d6cbe1594a6b998c36a6d
AnantDuhan <duhananant@gmail.com>
2021-06-13 18:02:40 +0530

    initial commit

---
+++ front-end-dev/src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSzRvUp6nucP2G8C3CT5eXDfrmmwZ8_Ho",
+    authDomain: "what-s-app-clone-d0b8e.firebaseapp.com",
+    projectId: "what-s-app-clone-d0b8e",
+    storageBucket: "what-s-app-clone-d0b8e.appspot.com",
+    messagingSenderId: "27260493701",
+    appId: "1:27260493701:web:0801974c9cc9c46a806eec",
+    measurementId: "G-8P1RV9W4FQ"
+};
