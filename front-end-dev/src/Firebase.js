b6d6fc66e603ed5b6b3f9a4a913012913fedddf8
AnantDuhan <duhananant@gmail.com>
2021-06-14 17:50:17 +0530

    ADD:Auth & db

--- front-end-dev/src/Firebase.js
+++ front-end-dev/src/Firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCSzRvUp6nucP2G8C3CT5eXDfrmmwZ8_Ho",
     authDomain: "what-s-app-clone-d0b8e.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
     appId: "1:27260493701:web:0801974c9cc9c46a806eec",
     measurementId: "G-8P1RV9W4FQ"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
