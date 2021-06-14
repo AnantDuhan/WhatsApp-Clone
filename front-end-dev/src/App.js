36d59312445d9e9cc54faa56a8f6d0955bac8d1e
AnantDuhan <duhananant@gmail.com>
2021-06-15 00:12:53 +0530

    Connected with components

--- front-end-dev/src/App.js
+++ front-end-dev/src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
+import Login from './Login';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useStateValue } from './StateProvider';
 
 function App() {
+
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
-      <div className="app__body">
-        {/* Sidebar */}
-        <Sidebar />
-        {/* Chat */}
-
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Sidebar />
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Sidebar />
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
